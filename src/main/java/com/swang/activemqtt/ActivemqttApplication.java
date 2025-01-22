bc5c8ade95f719afee724cfe2a671e8da095a636
ming.wang4 <ming.wang4@envision-digital.com>
2025-01-22 11:43:26 +0800

    init

---
+++ src/main/java/com/swang/activemqtt/ActivemqttApplication.java
@@ -0,0 +1,13 @@
+package com.swang.activemqtt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivemqttApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivemqttApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/swang/activemqtt/ActivemqttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swang.activemqtt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqttApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
